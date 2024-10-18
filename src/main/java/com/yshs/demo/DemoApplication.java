a1c62f7b6e2cb643d307cd0d1a26599aef9b4925
bling-yshs <377178599@qq.com>
2024-10-18 13:06:47 +0800

    2

--- src/main/java/com/yshs/demo/DemoApplication.java
+++ src/main/java/com/yshs/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot2MavenDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot2MavenDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
--- src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-create table student(
-    id long primary key,
-    name varchar(255),
-    age int
-)
